17c1c2fbca6fa0f171b7e763fc0fcfdc59e93d70
AldoAbdn <alistairaldo@hotmail.co.uk>
2019-10-21 20:18:14 +0100

    Initial File Upload

---
+++ Assets/Scipts/PlayerSelectMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerSelectMenu : MonoBehaviour
+{
+    static public int NumPlayers = 1;
+
+    public void StartButtonClick()
+    {
+        // Load game scene
+        SceneManager.LoadScene(1);
+    }
+
+    public void PlayerSliderOnValueChanged()
+    {
+
+    }
+}
